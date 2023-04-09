169e74d4eb60664ed22d34d9ec1c692e5889d6c4
Viktorya <79201287827@yandex.ru>
2023-04-09 10:49:18 +0300

    Задача 66

--- Program.cs
+++ Program.cs
@@ -1,17 +1,23 @@
-﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все
-//натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
-//N = 5 -> "5, 4, 3, 2, 1"
-//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
 
-Console.WriteLine("Введите натуральное число больше 1: ");
-int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите начальное число M:");
+int numberM = Convert.ToInt32(Console.ReadLine());
 
-void NumberCounter (int number)
+Console.WriteLine("Введите начальное число N:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+void GapNumberSum (int numberM, int numberN, int sum)
 {
-    if (number < 0) Console.Write($"{number} не натуральное число");
-    if (number == 0) return;
-    Console.Write("{0,4}", number);
-    NumberCounter (number - 1);
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
 }
 
-NumberCounter(number);
+GapNumberSum(numberM, numberN, 0);
